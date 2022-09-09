57ac08dc45d99bbcf67d198602dd034e5dde2926
Hamza056 <97276848+Hamza056@users.noreply.github.com>
2022-09-09 11:56:18 +0500

    second-commit

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ student=[
             id:[3], 
            
             TotalMark:150,
-            status:'pass',
+            status:'fail',
             opMarks:140
             },
             { 
@@ -50,7 +50,7 @@ student=[
                     },
                     { 
                         Name:'kreem',
-                        id:[12], 
+                        id:[7], 
                        
                         TotalMark:150,
                         
@@ -115,7 +115,7 @@ const displaymv=function(mv){
    
   
    const index3rd= mv.findIndex(function(todo,index){
-    return todo.opMarks>130&&todo.status=='pass' ;
+    return todo.opMarks>110&&todo.status=='pass' ;
      
    })
    console.log(mv[index3rd]);
@@ -135,7 +135,97 @@ const displaymv=function(mv){
      
    })
    console.log(mv[indexbtm3rd]);
+    
    const html=`
+   <div class='Records'>
+   <table>
+   <h1>Students Record Table</h1>
+   <tr>
+       <th>Id</th>
+       <th>Name</th>
+       <th>Obtained marks</th>
+       <th>Total Marks</th>
+       <th>status</th>
+   </tr>
+   <tr>
+       <td>${mv[0].id}</td>
+       <td>${mv[0].Name}</td>
+       <td>${mv[0].opMarks}</td>
+       <td>${mv[0].TotalMark}</td>
+       <td>pass</td>
+   </tr>
+   <tr>
+       <td>${mv[1].id}</td>
+       <td>${mv[1].Name}</td>
+       <td>${mv[1].opMarks}</td>
+       <td>${mv[1].TotalMark}</td>
+       <td>pass</td>
+   </tr>
+   <tr>
+       <td>${mv[2].id}</td>
+       <td>${mv[2].Name}</td>
+       <td>${mv[2].opMarks}</td>
+       <td>${mv[2].TotalMark}</td>
+       <td>pass</td>
+   </tr>
+   <tr>
+   <td>${mv[3].id}</td>
+   <td>${mv[3].Name}</td>
+   <td>${mv[3].opMarks}</td>
+   <td>${mv[3].TotalMark}</td>
+   <td>pass</td>
+  </tr>
+  <tr>
+  <td>${mv[4].id}</td>
+  <td>${mv[4].Name}</td>
+  <td>${mv[4].opMarks}</td>
+  <td>${mv[4].TotalMark}</td>
+  <td>pass</td>
+</tr>
+<tr>
+<td>${mv[5].id}</td>
+<td>${mv[5].Name}</td>
+<td>${mv[5].opMarks}</td>
+<td>${mv[5].TotalMark}</td>
+<td>pass</td>
+</tr>
+<tr>
+<td>${mv[6].id}</td>
+<td>${mv[6].Name}</td>
+<td>${mv[6].opMarks}</td>
+<td>${mv[6].TotalMark}</td>
+<td>pass</td>
+</tr>
+<tr>
+<td>${mv[7].id}</td>
+<td>${mv[7].Name}</td>
+<td>${mv[7].opMarks}</td>
+<td>${mv[7].TotalMark}</td>
+<td>pass</td>
+</tr>
+<tr>
+<td>${mv[8].id}</td>
+<td>${mv[8].Name}</td>
+<td>${mv[8].opMarks}</td>
+<td>${mv[8].TotalMark}</td>
+<td>pass</td>
+</tr>
+<tr>
+<td>${mv[9].id}</td>
+<td>${mv[9].Name}</td>
+<td>${mv[9].opMarks}</td>
+<td>${mv[9].TotalMark}</td>
+<td>pass</td>
+</tr>
+<tr>
+<td>${mv[10].id}</td>
+<td>${mv[10].Name}</td>
+<td>${mv[10].opMarks}</td>
+<td>${mv[10].TotalMark}</td>
+<td>pass</td>
+</tr>
+</table>
+   </div>
    <div class="heigh">
    <table>
        <h1>Top Three achievers</h1>
@@ -206,12 +296,7 @@ const displaymv=function(mv){
 </div>
    `
    stdtable.insertAdjacentHTML('afterbegin',html);
-// mv.forEach(function(mov,i){
-    
-//     console.log(Math.max(mov.opMarks));
-  
-// })
-//    console.log(mv);
+
 
   };
   displaymv(student);
